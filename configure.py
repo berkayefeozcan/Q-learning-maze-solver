1c9a763270908753f8b540613420b9f3fbb6525b
berkayefeozcan <berkayefeozcan@gmail.com>
2021-05-16 15:50:59 +0300

    cok sey

---
+++ configure.py
@@ -0,0 +1,15 @@
+# Setting the sizes for the environment
+pixels = 20   # pixels
+env_height = 25  # grid height
+env_width = 25  # grid width
+maxIndex = min(env_height,env_width) 
+# ratio must be max 1  
+randomPixelRatio = 0.3
+# Global variable for dictionary with coordinates for the final route
+a = {}
+# episodeAmount 
+episodeAmount =1000
+startPageTitle ='Home Page'
+startPageResolation="450x300" 
+startPositionCBOptions=(0,3,5,6,7,8,9,15)
+endPostionCBOptions = (0,3,5,6,7,8,9,24)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import numpy as np
 import tkinter as tk
 from tkinter import ttk
@@ -5,37 +6,46 @@ import time  # Time is needed to slow down the agent and to see how he runs
 from PIL import Image, ImageTk  # For adding images into the canvas widget
 from path import Environment
 from agent import QLearningTable
-
+from configure import episodeAmount,maxIndex,env_height,env_width,randomPixelRatio,startPageTitle,startPageResolation,startPositionCBOptions,endPostionCBOptions
 class HomePage(tk.Tk, object):
     def __init__(self):
         super(HomePage, self).__init__()       
         #self.configure(bg="white")
-        self.startPosition = tk.StringVar(value=0)
-        self.endPosition = tk.StringVar(value=0)
-        self.comboBoxOptions = (0,3,5,6,7,8,9)
-        self.title('Home Page')
-        self.geometry("450x300")
+        self.startPosition = [tk.StringVar(value=0),tk.StringVar(value=0)]
+        self.endPosition = [tk.StringVar(value=0),tk.StringVar(value=0)]
+        self.title(startPageTitle)
+        self.geometry(startPageResolation)
         self.create_widgets()
 
     def create_widgets(self):
         self.startButton =  tk.Button(self,text="baslat",command=self.startQLearning)
-        self.startButton.place(x = 40,y = 130)    
+        self.startButton.place(x = 190,y = 130)    
         self.startPositionLabel= tk.Label(self,text = "Başlangıç konumu :").place(x = 40,y = 60)
         self.endPositionLabel = tk.Label(self,text="Bitiş Konumu          :").place(x=40,y=100)
-        self.startPositionComboBox = ttk.Combobox(self,values=self.comboBoxOptions,textvariable=self.startPosition,width=5)
-        self.startPositionComboBox.place(x=150,y=60)
-        self.endPositionComboBox = ttk.Combobox(self,values=self.comboBoxOptions,textvariable=self.endPosition,width=5)
-        self.endPositionComboBox.place(x=150,y=100)
+        self.startPositionComboBoxX = ttk.Combobox(self,values=startPositionCBOptions,textvariable=self.startPosition[0],width=5)
+        self.startPositionComboBoxX.place(x=150,y=60)
+        self.startPositionComboBoxY = ttk.Combobox(self,values=startPositionCBOptions,textvariable=self.startPosition[1],width=5)
+        self.startPositionComboBoxY.place(x=210,y=60)
+        self.endPositionComboBoxX = ttk.Combobox(self,values=endPostionCBOptions,textvariable=self.endPosition[0],width=5)
+        self.endPositionComboBoxX.place(x=150,y=100)
+        self.endPositionComboBoxY = ttk.Combobox(self,values=endPostionCBOptions,textvariable=self.endPosition[1],width=5)
+        self.endPositionComboBoxY.place(x=210,y=100)
 
     def startQLearning(self):
-        print('start= {0} end ={1}'.format(type(self.startPosition.get()),type(self.endPosition.get())) )
         self.destroy()
-        startPixel = int(self.startPosition.get())
-        finishPixel = int(self.endPosition.get())
-        self.env = Environment(startPixel ,finishPixel)
-    # Calling for the main algorithm
+        startPixel = [int(self.startPosition[0].get()),int(self.startPosition[1].get())]
+        finishPixel = [int(self.endPosition[0].get()),int(self.endPosition[1].get())]
+        # random Obstacle coordinat list ex:5x5 [0,3]
+        obstacleCoordinats = np.random.randint(maxIndex,size=(int(env_height*env_width*randomPixelRatio),2))
+        for XY in obstacleCoordinats:
+            if (XY[0]==finishPixel[0] and XY[1]==finishPixel[1]) or (XY[0]==startPixel[0] and XY[1]==startPixel[1]):
+                print(obstacleCoordinats)
+                np.delete(obstacleCoordinats,XY)
+                print(obstacleCoordinats)
+        self.env = Environment(startPixel ,finishPixel,obstacleCoordinats)
+        # Calling for the main algorithm
         self.RL = QLearningTable(actions=list(range(self.env.n_actions)))
-    # Running the main loop with Episodes by calling the function update()
+        # Running the main loop with Episodes by calling the function update()
         self.env.after(100, self.update)  # Or just update()
         self.env.mainloop()
 
@@ -46,10 +56,9 @@ class HomePage(tk.Tk, object):
     # Summed costs for all episodes in resulted list
         all_costs = []
 
-        for episode in range(1000):
+        for episode in range(episodeAmount):
         # Initial Observation
             observation = self.env.reset()
-
         # Updating number of Steps for each Episode
             i = 0
 
